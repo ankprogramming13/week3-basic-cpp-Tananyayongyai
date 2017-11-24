95e5814fc980fdabea2baf6f802e6f26218e1506
Unknown <peatananya@gmail.com>
2017-11-24 14:25:14 +0700

    ทำข้อ 1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,5 +4,6 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
+  cout << "Miss." << "Tananya" << endl << "Yongyai" << endl << "Pea" << endl << "female" << endl << "3 July 1998" << endl << "reading fiction" << endl << "single" ;
   return 0;
 }
